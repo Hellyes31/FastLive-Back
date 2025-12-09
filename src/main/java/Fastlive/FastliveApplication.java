a55e8617f7df01b461a5d4d74e0c272ece086459
Hellyes31 <elies.auguet@gmail.com>
2025-12-09 10:46:23 +0100

    chore: setup SpringBoot Java 21

---
+++ src/main/java/Fastlive/FastliveApplication.java
@@ -0,0 +1,13 @@
+package Fastlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fastlive/FastliveApplicationTests.java
@@ -0,0 +1,13 @@
+package Fastlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
